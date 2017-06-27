8100b93ebdab6ce067f35021236641167a03c49c
arnoldthchan <arnoldthchan@gmail.com>
2017-06-27 23:44:11 +0000

    fixed palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,17 @@
 function isPalindrome(str) {
-  var noSpaces = str.split(" ").join("");
+  var noSpaces = str.split(" ").join("").toLowerCase();
   var mid = Math.floor(noSpaces.length / 2);
   var last = noSpaces.length - 1;
 
-  if (noSpaces.length = 1) return true;
-  for (var i = 0; i < mid; i++) {
-    if (str[i] === str[last - i]) return true;
-      console.log(i);
-      console.log('COMPARING', str[i], 'and', str[last-i]);
-      console.log(str[i] === str[last - i]);
+  for (var i = 0; i <= mid; i++) {
+    if (noSpaces[i] !== noSpaces[last - i]){
+
+      return false;
+    }
   }
+  return true;
 }
+
 
 // Test driver code. These should all log true.
 console.log(isPalindrome('p') === true);
